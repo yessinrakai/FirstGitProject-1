6a438d9b4adf13761a4d19044b17bda9dcc627f6
AleksandarBrkovic <aleksandarbrkovic47@gmail.com>
2021-11-17 11:01:22 +0100

    Added antoher comment from rep 1

--- letsgo.c
+++ letsgo.c
@@ -1,6 +1,6 @@
 /*added coomment from repository 1 (machine 1)*/
 #include <stdio.h>
-int main(){
+int main(){ //aggoiunto ancorz un commento dalla prima macchina 
 	printf("Git,let's go!!\n");
 	printf("Added line 1!\n");
 	printf("Working on a new feature 1...\n");
