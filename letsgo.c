79eb565861d0cc6cc1b60cbdbaa85c8cab432902
AleksandarBrkovic <aleksandarbrkovic47@gmail.com>
2021-11-17 11:03:53 +0100

    Added another comment from rep 2

--- letsgo.c
+++ letsgo.c
@@ -1,6 +1,6 @@
 /*added coomment from repository 1 (machine 1)*/
 #include <stdio.h>
-int main(){
+int main(){ //ancora un commento dall repository 2 macchina 2
 	printf("Git,let's go!!\n");
 	printf("Added line 1!\n");
 	printf("Working on a new feature 1...\n");
